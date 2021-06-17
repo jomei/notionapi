6ced706277cd965fea8ec997f35650602dd2801c
Reda Laanait <laanait.reda@gmail.com>
2021-06-17 12:11:57 +0100

    omit empty Property field in SortObject

--- sort.go
+++ sort.go
@@ -5,7 +5,7 @@ type SortOrder string
 type TimestampType string
 
 type SortObject struct {
-	Property  string        `json:"property"`
+	Property  string        `json:"property,omitempty"`
 	Timestamp TimestampType `json:"timestamp"`
 	Direction SortOrder     `json:"direction"`
 }
