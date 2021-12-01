e28680256086b6c021bbb0034f4a902f75683822
Misha Melnyk <as.k.github@melnyk.org>
2021-12-01 18:10:10 -0500

    Added parent field to database type

--- database.go
+++ database.go
@@ -110,6 +110,7 @@ type Database struct {
 	CreatedTime    time.Time  `json:"created_time"`
 	LastEditedTime time.Time  `json:"last_edited_time"`
 	Title          []RichText `json:"title"`
+	Parent         Parent     `json:"parent"`
 	// Properties is a map of property configurations that defines what Page.Properties each page of the database can use
 	Properties PropertyConfigs `json:"properties"`
 }
