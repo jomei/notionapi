d9ace692520ea9e9913b1ba96941ed89873ce30b
Robert Thomas <31854736+wolveix@users.noreply.github.com>
2025-10-01 14:48:10 +0100

    Support data sources

--- search.go
+++ search.go
@@ -16,16 +16,16 @@ type SearchClient struct {
 	apiClient *Client
 }
 
-// Searches all parent or child pages and databases that have been shared with
+// Searches all parent or child pages and data sources that have been shared with
 // an integration.
 //
-// Returns all pages or databases, excluding duplicated linked databases, that
+// Returns all pages or data sources, excluding duplicated linked data sources, that
 // have titles that include the query param. If no query param is provided, then
-// the response contains all pages or databases that have been shared with the
+// the response contains all pages or data sources that have been shared with the
 // integration. The results adhere to any limitations related to an integration’s
 // capabilities.
 
-// To limit the request to search only pages or to search only databases, use
+// To limit the request to search only pages or to search only data sources, use
 // the filter param.
 //
 // See https://developers.notion.com/reference/post-search
@@ -51,7 +51,7 @@ func (sc *SearchClient) Do(ctx context.Context, request *SearchRequest) (*Search
 }
 
 type SearchRequest struct {
-	// The text that the API compares page and database titles against.
+	// The text that the API compares page and data source titles against.
 	Query string `json:"query,omitempty"`
 	// A set of criteria, direction and timestamp keys, that orders the results.
 	// The only supported timestamp value is "last_edited_time". Supported
@@ -59,8 +59,8 @@ type SearchRequest struct {
 	// then the most recently edited results are returned first.
 	Sort *SortObject `json:"sort,omitempty"`
 	// A set of criteria, value and property keys, that limits the results to
-	// either only pages or only databases. Possible value values are "page" or
-	// "database". The only supported property value is "object".
+	// either only pages or only data sources. Possible value values are "page" or
+	// "data_source". The only supported property value is "object".
 	Filter SearchFilter `json:"filter,omitempty"`
 	// A cursor value returned in a previous response that If supplied, limits the
 	// response to results starting after the cursor. If not supplied, then the
@@ -85,30 +85,31 @@ func (sr *SearchResponse) UnmarshalJSON(data []byte) error {
 		NextCursor Cursor        `json:"next_cursor"`
 	}
 
-	err := json.Unmarshal(data, &tmp)
-	if err != nil {
+	if err := json.Unmarshal(data, &tmp); err != nil {
 		return err
 	}
+
 	objects := make([]Object, len(tmp.Results))
 	for i, rawObject := range tmp.Results {
 		var o Object
 		switch rawObject.(map[string]interface{})["object"].(string) {
-		case ObjectTypeDatabase.String():
-			o = &Database{}
+		case ObjectTypeDataSource.String():
+			o = &DataSource{}
 		case ObjectTypePage.String():
 			o = &Page{}
 		default:
 			return fmt.Errorf("unsupported object type %s", rawObject.(map[string]interface{})["object"].(string))
 		}
+
 		j, err := json.Marshal(rawObject)
 		if err != nil {
 			return err
 		}
 
-		err = json.Unmarshal(j, o)
-		if err != nil {
+		if err = json.Unmarshal(j, o); err != nil {
 			return err
 		}
+
 		objects[i] = o
 	}
 
